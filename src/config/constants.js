f22813a324cf56fa16973c413731ac48756ad734
alireza <alirezasoheili1224@yahoo.com>
2023-04-26 23:20:38 +0330

    add production url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 export const __isProd__ = process.env.NODE_ENV === 'production'
-export const URL = __isProd__ ? 'https://isupport-backned.onrender.com/api' : 'http://localhost:8000/api';
+export const URL = !__isProd__ ? 'https://isupport-backned.onrender.com/api' : 'http://localhost:8000/api';
